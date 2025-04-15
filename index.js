3f36797383c9097da836ce89af3eb465582c5362
RaphaelIsaiah <rafvie12@gmail.com>
2025-04-15 19:14:41 +0100

    refactor: update file paths and clean up unused CSS variables for improved maintainability

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // init project
 var express = require("express");
+const path = require("path");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -11,11 +12,13 @@ var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  // res.sendFile(__dirname + "/views/index.html");G
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 // your first API endpoint...
